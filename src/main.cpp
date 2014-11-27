18f4ceb678187b03c88f68aae9d533cb1a2292fc
Riyyi <riyyi3@gmail.com>
2014-11-27 20:45:16 +0100

    Go through parameters in order

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,9 @@ int main(int argc, char ** argv) {
         // Declare salt variable
         char salt[] = "$6$[ENTER YOUR SALT HERE]$";
 
-        // Help argument
-        int help; // -h --help
+        // Arguments
+        int help;       // -h --help
+        int arglength;  // -l --length [number]
 
     // Construct GetOpt object by passing argc and argv arguments
     GetOpt::GetOpt_pp args(argc, argv);
@@ -23,14 +24,17 @@ int main(int argc, char ** argv) {
 
     // Process arguments
     help = args >> GetOpt::OptionPresent('h', "help");
+    arglength = args >> GetOpt::OptionPresent('l', "length");
 
-    try {
-        args >> GetOpt::Option('l', "length", length);
-    }
-    catch (GetOpt::GetOptEx) {
-        std::cerr << "nopassdb: value given to option -l --length wasn't a number." << std::endl;
+    if (arglength) {
+        try {
+            args >> GetOpt::Option('l', "length", length);
+        }
+        catch (GetOpt::GetOptEx) {
+            std::cerr << "nopassdb: value given to option -l --length wasn't a number." << std::endl;
 
-        return 0;
+            return 0;
+        }
     }
 
     if (args.options_remain()) {
@@ -40,29 +44,37 @@ int main(int argc, char ** argv) {
     }
 
     // Go through options
-    if (help) {
-        ArgHelp arghelp;
-        arghelp.Main();
+    std::vector<std::string> argvector(argv, argv+argc);
 
-        return 0;
+    for (std::string arg : argvector) {
+        if (help && (arg == "-h" || arg == "--help")) {
+            ArgHelp arghelp;
+            arghelp.Main();
+
+            return 0;
+        }
+
+        if (arglength  && (arg == "-l" || arg == "--length")) {
+            // Construct password hashing object
+            Hash hash;
+
+            // Get users password
+            char *password;
+            password = hash.GetPassword();
+
+            int start = std::strlen(salt);
+
+            // Hash password
+            std::string output;
+            output = hash.HashPassword(password, salt);
+
+            output = hash.OutputLength(output, start, length);
+            std::cout << output << std::endl;
+
+            return 0;
+        }
     }
-    else {
-        // Construct password hashing object
-        Hash hash;
 
-        // Get users password
-        char *password;
-        password = hash.GetPassword();
-
-        int start = std::strlen(salt);
-
-        // Hash password
-        std::string output;
-        output = hash.HashPassword(password, salt);
-
-        output = hash.OutputLength(output, start, length);
-        std::cout << output << std::endl;
-    }
 
     return 0;
 }
